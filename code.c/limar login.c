68235a4acdb7a907b1bde203c1b8e51e0114505d
ChristopherDevon <devonchen12@gmail.com>
2018-07-08 19:17:44 +0700

    source code

--- code.c/limar login.c
+++ code.c/limar login.c
@@ -21,7 +21,7 @@ int main(){
 	do{
 		printf("\nWhich one would you like to do ?");
 		printf("\n 1 : Addition \n 2 : Subtraction \n 3 : Multiplication \n 4 : Division \n 5 : Modulus \n Type the number of the operation : ");
-		scanf("%d", &op);
+		scanf("%d", &op);getchar();
 	
 	}while
 		((op != 1) && (op != 2) && (op != 3) && (op != 4) && (op != 5));
@@ -29,36 +29,39 @@ int main(){
 
 	float value;
 	
-	if (op = 1)	
+	if (op == 1)	
 	{	printf ("How much money do you want to add to your account ? \nAmount of Money : ");
-		scanf ("%f,", &value);
+		scanf ("%f,", &value);getchar();
 		balance = balance + value ; 
 		printf ("Your balance after addition is : Rp.%.2f,\n",balance);
 	}
-	else if (op = 2)
+	else if (op == 2)
 	{
 		printf ("How much money do you want to take from your account ? \nAmount of Money : ");
-		scanf ("%f",&value);
+		scanf ("%f",&value);getchar();
 		balance = balance - value; 
-		printf ("Your balance after addition is : Rp.%.2f\n",balance);
+		printf ("Your balance after subtraction is : Rp.%.2f\n",balance);
 	}
-	else if (op = 3)
+	else if (op == 3)
 	{
 		printf ("How much money do you want to multiply from your account ? \nAmount of Money : ");
-		scanf ("%f",&value);
+		scanf ("%f",&value);getchar();
 		balance = balance * value;
-		printf ("Your balance after addition is : Rp.%.2f\n",balance);
+		printf ("Your balance after multiplication is : Rp.%.2f\n",balance);
 	}
-	else if (op = 4)
+	else if (op == 4)
 	{
 		printf ("How much money do you want to divide from your account ? \nAmount of Money : ");
-		scanf ("%f",&value);
+		scanf ("%f",&value);getchar();
 		balance = balance / value; 
-		printf ("Your balance after addition is : Rp.%.2f\n",balance);
+		printf ("Your balance after division is : Rp.%.2f\n",balance);
 	}
-	else if (op = 5)
+	else if (op == 5)
 	{
 		printf("what value ? \n");
-		scandf("%f",)
+		scanf("%f",&value);getchar();
+		balance = fmod(balance,value);
+		printf("your balance is now %.2f\n\n");
 	}
+	
 }
